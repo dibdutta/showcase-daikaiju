7d541125f83150a0bbf44f9ff453c453bfd4778b
Dibyendu Dutta <dutta.dibyendu2010@gmail.com>
2026-02-11 08:52:19 +0530

    fix usps issue

--- src/usps/usps_calculator.php
+++ src/usps/usps_calculator.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(E_ALL ^ E_NOTICE);  
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED);
 function USPSParcelRate($dest_zip,$width, $height, $length, $weight_lb,$weight_oz)
 {
 
