acb2797aa934be59f89cc1e66f8f33b211a025bd
Dibyendu Dutta <dutta.dibyendu2010@gmail.com>
2026-03-20 23:14:18 +0530

    Added USPS debug

--- src/usps/usps.php
+++ src/usps/usps.php
@@ -10,11 +10,8 @@ $i=0;
 $Price=0.00;
 $err_ind=false;
 foreach($weight_arr as $key =>$val){
-	// $sql_fetch_shipping_details=" Select * from tbl_size_weight_cost_master
-	// 								 where size_weight_cost_id = ".$weight_arr[$i];
-
 	$sql_fetch_shipping_details=" Select * from tbl_size_weight_cost_master
-	 								 where size_weight_cost_id = 10";
+	 								 where size_weight_cost_id = ".(int)$weight_arr[$i];
 
 	
 	$sql_fetch_shipping_details_res=mysqli_fetch_array(mysqli_query($GLOBALS['db_connect'],$sql_fetch_shipping_details));
