89a467a4bcf9c55e3f347730053708d2b65bf009
Dibyendu Dutta <dutta.dibyendu2010@gmail.com>
2026-04-03 09:29:23 +0530

    Restore silent error reporting, suppress Smarty deprecation warnings

--- src/lib/configures.php
+++ src/lib/configures.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+error_reporting(E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR);
+ini_set('display_errors', '0');
 
 require_once "dbconfig.php";
 require_once "var.inc.php";
