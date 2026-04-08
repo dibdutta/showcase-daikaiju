1060beb98ae03f2327f4f3037c00ed04dafc679b
Dibyendu Dutta <dutta.dibyendu2010@gmail.com>
2026-04-08 22:14:44 +0530

    debug

--- src/ajax.php
+++ src/ajax.php
@@ -5,6 +5,7 @@ header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 define ("INCLUDE_PATH", "");
 require_once INCLUDE_PATH."lib/inc.php";
+ini_set('display_errors','1'); error_reporting(E_ALL);
 
 if($_REQUEST['mode'] == "delete_poster"){
 	delete_poster();
